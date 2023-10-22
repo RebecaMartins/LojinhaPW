b5b969f714306404a48389fb0c46cbbda5e7335a
RebecaMartins <128054719+RebecaMartins@users.noreply.github.com>
2023-10-21 21:55:22 -0300

    Delete conexao.php
    aulas 32 a 37

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $servidor = "Localhost";
-   $usuario = "lojinha";
-   $senha = "123456";
-   $banco = "dbLojinha"
-
-$cn = new PDO ("mysql:host=$servidor;dbname=$banco, $usuario, $senha");
-
-?>
